edfda2720e9dceaa51d8c69fb85c0875fe2bb7d2
J. David Beutel <git@getsu.com>
2017-09-09 11:30:07 -1000

    changed VolunteerControllerFunctionalSpec to use @OnceBefore.
    Inserting and deleting the db test fixture once per spec runs faster (I assume) than doing it for each feature (in setup() and cleanup()).  Of course, this speedup is insignificant.  I just did it to try out what I think will be a typical pattern for Geb @Stepwise specs (although they wouldn't use @Rollback).

--- src/integration-test/groovy/alohavolunteer/VolunteerControllerFunctionalSpec.groovy
+++ src/integration-test/groovy/alohavolunteer/VolunteerControllerFunctionalSpec.groovy
@@ -4,6 +4,8 @@ import geb.spock.GebReportingSpec
 import grails.core.GrailsApplication
 import grails.testing.mixin.integration.Integration
 import grails.gorm.transactions.Rollback
+import grails.testing.spock.OnceBefore
+import spock.lang.Shared
 
 
 /**
@@ -13,22 +15,32 @@ import grails.gorm.transactions.Rollback
 @Rollback
 class VolunteerControllerFunctionalSpec extends GebReportingSpec {
 
-    Volunteer example
+    @Shared Volunteer example
     GrailsApplication grailsApplication
 
-    void setup() {
-        // Apparently need to save example here, outside the @Rollback test transaction,
-        // to have the app in the browser see it in the db.  Also, apparently need to
-        // do this for each test, because Grails does not seem to be ready for setupSpec(),
-        // even with @Stepwise.
+    @OnceBefore
+    void setupData() {
+        // Setup the data once for the whole spec, just to run a little faster.
+        // We can't do this in setupSpec(), because GORM isn't initialized by then
+        // (even with @Stepwise, which is often used with Geb).  This is more of
+        // a demo of the typical use with Geb @Stepwise (without @Rollback).
+
+        // This could also be done in setup(), for each feature.  However, because of this
+        // spec's GORM @Rollback, it cannot be done in a feature, even in the setup: clause.
+        // With @Rollback, the feature's test transaction cannot be committed,
+        // so the app in the browser could not see its data.
+
         example = VolunteerSpec.constructExampleVolunteer()
         example.save()
     }
 
-    void cleanup() {
+    void cleanupSpec() {
         // Need to explicitly cleanup example,
         // because it was saved outside the @Rollback test transaction.
-        example.delete()
+        // But, Grails doesn't provide a session to cleanupSpec(), so we create our own.
+        Volunteer.withNewSession {
+            example.delete(flush: true)     // flush is required to actually delete, although I don't know why
+        }
     }
     
     void "config for @Integration specs is consistent, with profiles overriding"() {
