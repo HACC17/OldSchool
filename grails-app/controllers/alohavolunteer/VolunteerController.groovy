9254ab1df85055e241a7763245a1e2c2a6d05f3e
J. David Beutel <git@getsu.com>
2017-09-15 20:20:22 -1000

    grails generate-all Volunteer -force
    to generate static scaffolding, instead of dynamic, to see and customize

--- grails-app/controllers/alohavolunteer/VolunteerController.groovy
+++ grails-app/controllers/alohavolunteer/VolunteerController.groovy
@@ -1,6 +1,107 @@
 package alohavolunteer
 
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
 class VolunteerController {
 
-    static scaffold = Volunteer
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Volunteer.list(params), model:[volunteerCount: Volunteer.count()]
+    }
+
+    def show(Volunteer volunteer) {
+        respond volunteer
+    }
+
+    def create() {
+        respond new Volunteer(params)
+    }
+
+    @Transactional
+    def save(Volunteer volunteer) {
+        if (volunteer == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (volunteer.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond volunteer.errors, view:'create'
+            return
+        }
+
+        volunteer.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'volunteer.label', default: 'Volunteer'), volunteer.id])
+                redirect volunteer
+            }
+            '*' { respond volunteer, [status: CREATED] }
+        }
+    }
+
+    def edit(Volunteer volunteer) {
+        respond volunteer
+    }
+
+    @Transactional
+    def update(Volunteer volunteer) {
+        if (volunteer == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (volunteer.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond volunteer.errors, view:'edit'
+            return
+        }
+
+        volunteer.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'volunteer.label', default: 'Volunteer'), volunteer.id])
+                redirect volunteer
+            }
+            '*'{ respond volunteer, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Volunteer volunteer) {
+
+        if (volunteer == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        volunteer.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'volunteer.label', default: 'Volunteer'), volunteer.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'volunteer.label', default: 'Volunteer'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
 }
--- src/test/groovy/alohavolunteer/VolunteerControllerSpec.groovy
+++ src/test/groovy/alohavolunteer/VolunteerControllerSpec.groovy
@@ -1,27 +1,152 @@
 package alohavolunteer
 
-import grails.testing.gorm.DataTest
-import grails.testing.web.controllers.ControllerUnitTest
-import spock.lang.Specification
+import grails.test.mixin.*
+import spock.lang.*
 
-class VolunteerControllerSpec extends Specification implements ControllerUnitTest<VolunteerController>, DataTest {
+@TestFor(VolunteerController)
+@Mock(Volunteer)
+class VolunteerControllerSpec extends Specification {
 
-    void setupSpec() {
-        mockDomain Volunteer
+    def populateValidParams(params) {
+        assert params != null
+
+        // TODO: Populate valid properties like...
+        //params["name"] = 'someValidName'
+        assert false, "TODO: Provide a populateValidParams() implementation for this generated test suite"
     }
 
-    def setup() {
+    void "Test the index action returns the correct model"() {
+
+        when:"The index action is executed"
+            controller.index()
+
+        then:"The model is correct"
+            !model.volunteerList
+            model.volunteerCount == 0
     }
 
-    def cleanup() {
+    void "Test the create action returns the correct model"() {
+        when:"The create action is executed"
+            controller.create()
+
+        then:"The model is correctly created"
+            model.volunteer!= null
     }
 
-    // When the unit test framework mocks the controller,
-    // it doesn't look like the mock implements dynamic scaffolding,
-    // so there's nothing we can test at this level yet.
-    void "uses dynamic scaffolding"() {
+    void "Test the save action correctly persists an instance"() {
 
-        expect:
-        controller.scaffold == Volunteer
+        when:"The save action is executed with an invalid instance"
+            request.contentType = FORM_CONTENT_TYPE
+            request.method = 'POST'
+            def volunteer = new Volunteer()
+            volunteer.validate()
+            controller.save(volunteer)
+
+        then:"The create view is rendered again with the correct model"
+            model.volunteer!= null
+            view == 'create'
+
+        when:"The save action is executed with a valid instance"
+            response.reset()
+            populateValidParams(params)
+            volunteer = new Volunteer(params)
+
+            controller.save(volunteer)
+
+        then:"A redirect is issued to the show action"
+            response.redirectedUrl == '/volunteer/show/1'
+            controller.flash.message != null
+            Volunteer.count() == 1
+    }
+
+    void "Test that the show action returns the correct model"() {
+        when:"The show action is executed with a null domain"
+            controller.show(null)
+
+        then:"A 404 error is returned"
+            response.status == 404
+
+        when:"A domain instance is passed to the show action"
+            populateValidParams(params)
+            def volunteer = new Volunteer(params)
+            controller.show(volunteer)
+
+        then:"A model is populated containing the domain instance"
+            model.volunteer == volunteer
+    }
+
+    void "Test that the edit action returns the correct model"() {
+        when:"The edit action is executed with a null domain"
+            controller.edit(null)
+
+        then:"A 404 error is returned"
+            response.status == 404
+
+        when:"A domain instance is passed to the edit action"
+            populateValidParams(params)
+            def volunteer = new Volunteer(params)
+            controller.edit(volunteer)
+
+        then:"A model is populated containing the domain instance"
+            model.volunteer == volunteer
+    }
+
+    void "Test the update action performs an update on a valid domain instance"() {
+        when:"Update is called for a domain instance that doesn't exist"
+            request.contentType = FORM_CONTENT_TYPE
+            request.method = 'PUT'
+            controller.update(null)
+
+        then:"A 404 error is returned"
+            response.redirectedUrl == '/volunteer/index'
+            flash.message != null
+
+        when:"An invalid domain instance is passed to the update action"
+            response.reset()
+            def volunteer = new Volunteer()
+            volunteer.validate()
+            controller.update(volunteer)
+
+        then:"The edit view is rendered again with the invalid instance"
+            view == 'edit'
+            model.volunteer == volunteer
+
+        when:"A valid domain instance is passed to the update action"
+            response.reset()
+            populateValidParams(params)
+            volunteer = new Volunteer(params).save(flush: true)
+            controller.update(volunteer)
+
+        then:"A redirect is issued to the show action"
+            volunteer != null
+            response.redirectedUrl == "/volunteer/show/$volunteer.id"
+            flash.message != null
+    }
+
+    void "Test that the delete action deletes an instance if it exists"() {
+        when:"The delete action is called for a null instance"
+            request.contentType = FORM_CONTENT_TYPE
+            request.method = 'DELETE'
+            controller.delete(null)
+
+        then:"A 404 is returned"
+            response.redirectedUrl == '/volunteer/index'
+            flash.message != null
+
+        when:"A domain instance is created"
+            response.reset()
+            populateValidParams(params)
+            def volunteer = new Volunteer(params).save(flush: true)
+
+        then:"It exists"
+            Volunteer.count() == 1
+
+        when:"The domain instance is passed to the delete action"
+            controller.delete(volunteer)
+
+        then:"The instance is deleted"
+            Volunteer.count() == 0
+            response.redirectedUrl == '/volunteer/index'
+            flash.message != null
     }
 }
