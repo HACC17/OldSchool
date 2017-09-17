8df41a4141acc206f488b3796274e0f31cac1661
J. David Beutel <git@getsu.com>
2017-09-16 23:01:48 -1000

    implemented webhook to allow config of Facebook Messenger app
    as described at https://developers.facebook.com/docs/messenger-platform/guides/quick-start
    
    This uses an environment variable that I configured on my Heroku instance of this app as follows:
    
    $ heroku config:set FACEBOOK_MESSENGER_VERIFY_TOKEN=XXX
    
    where XXX is the Verify Token that I have configured for my Facebook app's Messenger added product's Webhooks config.  With this, I will see if Facebook will verify and save https://guarded-atoll-72753.herokuapp.com/webhook as my Callback URL.  (Facebook has rejected that URL so far, because of bad responses.)

--- grails-app/controllers/alohavolunteer/WebhookController.groovy
+++ grails-app/controllers/alohavolunteer/WebhookController.groovy
@@ -1,6 +1,15 @@
 package alohavolunteer
 
+import javax.servlet.http.HttpServletResponse
+
 class WebhookController {
 
-    def index() { }
+    def index() {
+        def vt = grailsApplication.config['facebook.messenger.verifyToken']
+        if (params['hub.mode'] == 'subscribe' && params['hub.verify_token'] == vt) {
+            render text: params['hub.challenge']
+        } else {
+            render status: HttpServletResponse.SC_FORBIDDEN // 403
+        }
+    }
 }
--- src/test/groovy/alohavolunteer/WebhookControllerSpec.groovy
+++ src/test/groovy/alohavolunteer/WebhookControllerSpec.groovy
@@ -10,9 +10,31 @@ class WebhookControllerSpec extends Specification implements ControllerUnitTest<
 
     def cleanup() {
     }
+    
+    void "index without the proper params returns status 403"() {
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+        when:
+        controller.index()
+
+        then:
+        status == 403
+    }
+
+    void "subscribe request with proper params returns status 200 and challenge"() {
+
+        given:
+        final CHALLENGE = 'foo'
+        params << [
+                'hub.mode': 'subscribe',
+                'hub.verify_token': [:],
+                'hub.challenge': CHALLENGE
+        ]
+
+        when:
+        controller.index()
+
+        then:
+        status == 200
+        response.text == CHALLENGE
     }
 }
